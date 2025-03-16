40f77771c158c9f48e0ff2ad416015fbf51ecf3d
framirezj <12649259+framirezj@users.noreply.github.com>
2025-03-16 18:52:28 -0300

    created: funcionalidad completo aunque falta refactorizar

--- assets/js/mi_script.js
+++ assets/js/mi_script.js
@@ -13,9 +13,13 @@ Tip: En la guía de APIs hay un capítulo asociado a “Plugins de JavaScript (C
 Utiliza esto como referencia para generar el gráfico.
 */
 
-//obtener los datos desde la api
+//Variables
 const API_URL = "https://mindicador.cl/api/";
 const metadatos = ["version", "autor", "fecha"];
+const selectIndicadores = document.querySelector("#indicadores");
+const botonCalcular = document.querySelector("#calcular");
+const cantidadCLP = document.querySelector("#cantidad");
+const resultado = document.querySelector("#resultado");
 
 //Funcion para obtener los datos de la API
 const getDatos = async (url) => {
@@ -30,24 +34,61 @@ const getDatos = async (url) => {
 
 //Funcion para obtener un nuevo array con los indicadores, valores, unidad de medida.
 const getIndicadores = async () => {
-  
-    
   const data = await getDatos(API_URL);
 
   //obtener array de indicadores
   const indicadores = Object.keys(data)
-    .filter((indicador) => !metadatos.includes(indicador))
+    .filter(
+      (indicador) =>
+        !metadatos.includes(indicador) &&
+        data[indicador].unidad_medida !== "Porcentaje"
+    )
     .map((indicador) => ({
       nombre: data[indicador].nombre,
       valor: data[indicador].valor,
-      unidad: data[indicador].unidad_medida
+      unidad: data[indicador].unidad_medida,
     }));
   //const indicadores = Object.keys(data).filter( indicador => console.log(data[indicador].valor));
 
-    console.log("indicadores", indicadores)
-    return indicadores
-
+  console.log("indicadores", indicadores);
+  return indicadores;
 };
 
-getIndicadores();
+//Funcion para poner el listado de indicadores
+const listadoIndicadores = async () => {
+  const indicadores = await getIndicadores();
 
+  let template = "";
+  for (const indicador of indicadores) {
+    template += `
+        <option value="${indicador.valor}">${indicador.nombre}</option>
+    `;
+  }
+
+  selectIndicadores.innerHTML = template;
+};
+
+//Funcion al momento de apretar el boton para obtener el resultado
+botonCalcular.addEventListener("click", async () => {
+  //obtener la lista de indicadores y el valor del dolar observado
+  const valorDolar = (await getIndicadores()).find(
+    (indicador) => indicador.nombre === "Dólar observado"
+  ).valor;
+
+  const opcionSeleccionada =
+    selectIndicadores.options[selectIndicadores.selectedIndex];
+
+  if (opcionSeleccionada.textContent === "Bitcoin") {
+    const dolaresCLP = cantidadCLP.value / valorDolar;
+
+    const bitcoinCLP = dolaresCLP / selectIndicadores.value;
+    resultado.innerHTML = Math.trunc(bitcoinCLP * 1000000) / 1000000;
+  } else {
+    resultado.innerHTML =
+      Math.trunc((cantidadCLP.value / selectIndicadores.value) * 100) / 100;
+  }
+});
+
+//llamadas
+getIndicadores();
+listadoIndicadores();
